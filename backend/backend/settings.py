6ad7535f9ef704511062aface22c69ce398b00c8
dmitrii <mathmtit@gmail.com>
2025-08-17 21:17:24 -0700

    moved files in backed folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,9 +48,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-30f9pt)acw+2hidic&vktb0m(*$g676x0%(dno62#4+3@2rybi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 ALLOWED_PARTIES = ["http://localhost:5173"]  
 # Application definition
